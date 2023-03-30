86afaf4b1821eced7c7534a5551a7dbd55e6f4b7
[3sua3] <[suajks0219@naver.com]>
2023-03-30 19:10:18 +0900

    first commit

--- CountProject/CountApp/views.py
+++ CountProject/CountApp/views.py
@@ -9,4 +9,4 @@ def result(request):
     total_len = len(text)
     blank_exp_total_len = len(text.replace(" ", ""))
     total_word = len(text.split())
-    return render(request, 'result.html', {'total_len': total_len, 'text': text,'blank_exp_total_len': blank_exp_total_len, 'total_word': total_word})
+    return render(request, 'result.html', {'total_len': total_len, 'text': text,'blank_exp_total_len': blank_exp_total_len, 'total_word': total_word})  
